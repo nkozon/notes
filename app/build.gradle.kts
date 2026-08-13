576d57d25bd6439024a40c7a51a23c480a6ea03d
AminAtmani <aminatmaniabdelwahab@outlook.com>
2026-08-14 00:11:58 +0200

    Updated internal version to 1.2.2 for hotfix release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ozon.notes"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.1"
+        versionCode = 4
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
