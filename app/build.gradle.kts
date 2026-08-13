f23ebdffceeba49855d9eafbdde62e02fbdecc46
AminAtmani <aminatmaniabdelwahab@outlook.com>
2026-08-13 22:21:41 +0200

    Updated internal version to 1.2 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ozon.notes"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
