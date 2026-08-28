3f540c277de9b6d1a7ce507d7b5256274d2e7b0a
AminAtmani <aminatmaniabdelwahab@outlook.com>
2026-08-28 13:07:20 +0200

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ozon.notes"
         minSdk = 30
         targetSdk = 36
-        versionCode = 28
-        versionName = "1.10.2"
+        versionCode = 29
+        versionName = "1.10.3"
 
         val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
